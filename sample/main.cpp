83de390d2b4a2317e1f74346ab1b36d0f7cb07da
Moonslate <Andreymds88@gmail.com>
2023-02-01 20:05:23 -0300

    Update sample

--- sample/main.cpp
+++ sample/main.cpp
@@ -4,15 +4,17 @@
 #include <thread>
 #include <filesystem>
 
+std::filesystem::path db_path = std::filesystem::absolute("sample") / "database.db";
+
 class User : public uva::database::basic_active_record
 {    
     uva_database_declare(User);
     uva_database_expose_column(name);
     uva_database_expose_column(password);
+    uva_database_expose_column(age);
 };
 
 uva_database_define(User);
-uva_database_define_sqlite3(std::filesystem::absolute("db") / "database.db");
 
 class AddUsersMigration : public uva::database::basic_migration
 {
@@ -25,6 +27,7 @@ protected:
             { "id",         "INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL" },
             { "name",       "TEXT NOT NULL" },
             { "password",   "TEXT NOT NULL" },
+            { "age",        "INTEGER NOT NULL" },
             { "updated_at", "INTEGER NOT NULL DEFAULT (STRFTIME('%s'))" },
             { "created_at", "INTEGER NOT NULL DEFAULT (STRFTIME('%s'))" },
             { "removed",    "INTEGER NOT NULL DEFAULT 0" },
@@ -36,10 +39,19 @@ uva_define_migration(AddUsersMigration);
 
 int main()
 {
-   User user;
-   user.name = "Andrey";
-   user.password = "Wci010203";
-   user.save();
+    if(std::filesystem::exists(db_path)) {
+        std::filesystem::remove(db_path);
+    }
+
+    //Good to put under your app initialization
+    uva_database_define_sqlite3(db_path);
+    uva_run_migrations();
+
+    User user;
+    user.name = "Dummy";
+    user.password = "Some Password";
+    user.age = 21;
+    user.save();
 
     return 0;
 }
